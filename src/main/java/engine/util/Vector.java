1765ed056316be39ffe200db3c03e8758d9e5b11
bibleCampVictim09 <tzabar091@gmail.com>
2025-01-16 18:13:55 +0200

    created vector(big russian guy)

---
+++ src/main/java/engine/util/Vector.java
@@ -0,0 +1,19 @@
+package engine.util;
+
+public class Vector {
+
+    private int dist, dir;
+
+
+
+    public Vector() {}
+    public void setVector(int unitX,int unitY,int deltaX,int deltaY){
+
+        dist = (int) Math.sqrt(((deltaX-unitX) * (deltaX-unitX)) + (deltaY-unitY) * (deltaY-unitY));
+        dir = (int) Math.atan2(deltaY-unitY, deltaX-unitX);
+    }
+
+    public int getDist() {return dist;}
+    public int getDir() {return dir;}
+
+}
